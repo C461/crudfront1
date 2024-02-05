ed36c1f000a40213fbee6ad06cd97b66ed6d9c77
C461 <kozefra@gmai.com>
2024-02-04 23:01:51 -0500

    first commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,13 @@
-import axios from 'axios'
+import axios from "axios";
 
-const instance =axios.create({
-    baseURL: 'http://localhost:4000/api',
-    withCredentials: true,
-})
+const instance = axios.create({
+  baseURL: "https://crudfront1-production.up.railway.app",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin":
+      "https://crudfront1-production.up.railway.app/register https://crudfront1-production.up.railway.app/login",
+  },
+});
 
-export default instance 
+export default instance;
